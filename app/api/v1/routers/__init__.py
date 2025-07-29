0079981ab76468e57c654f986f34bca2c1f35a82
Soomxn <sooming@duksung.ac.kr>
2025-07-29 11:20:16 +0900

    [FEAT] 패턴 감지 라우터 및 엔드포인트 추가 #6

--- app/api/v1/routers/__init__.py
+++ app/api/v1/routers/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.routers import stock_router, backtest_router
+from app.api.v1.routers import stock_router, backtest_router, pattern_detection_router
 
 router = APIRouter()
 
 router.include_router(stock_router.router, prefix="/v1")
 router.include_router(backtest_router.router, prefix="/v1")
+router.include_router(pattern_detection_router.router, prefix="/v1")
 
 # 아래에 추가 ex. router.include_router(user_router.router, prefix="/v1")
