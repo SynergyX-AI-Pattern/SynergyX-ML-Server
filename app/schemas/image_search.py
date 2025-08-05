5df0b13b48d6e525056e78e08075fcd2b5b82407
Soomxn <sooming@duksung.ac.kr>
2025-08-05 19:50:37 +0900

    [FEAT] 이미지 검색용 스키마 생성 #16

---
+++ app/schemas/image_search.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ImageSearchResponse(BaseModel):
+    id: Optional[int]
+
+    model_config = {
+        "from_attributes": True,
+        "json_schema_extra": {
+            "example": {
+                "id": "1"
+            }
+        }
+    }
