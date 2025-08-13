be8719eb400eecc0c64a2cbd45688e2c5c82e730
Soomxn <sooming@duksung.ac.kr>
2025-08-13 14:00:56 +0900

    [CHORE] gpt, cloud api 키 추가 #16

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     ENV: Literal["local", "prod"] = "prod"  # 기본값 prod
     DISCORD_WEBHOOK_URL: str
     SPRING_SERVER_BASE_URL: HttpUrl
+    GOOGLE_APPLICATION_CREDENTIALS: str
+    OPENAI_API_KEY: str
 
     class Config:
         env_file = ".env"
