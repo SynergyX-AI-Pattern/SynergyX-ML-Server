5249356c16d856dfa6fa60b5cf8fe822e5956d91
Soomxn <sooming@duksung.ac.kr>
2025-08-13 13:35:11 +0900

    [FEAT] 이미지 서비스 엔드포인트 추가 #16

---
+++ app/api/v1/endpoints/stocks/image_search.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, UploadFile, File, Depends
+from app.db.session import get_db
+from sqlalchemy.orm import Session
+from app.api_payload.code.success_status import SuccessStatus
+from app.core.response import success_response
+from app.schemas.base_response import BaseResponse
+from app.schemas.image_search import ImageSearchResponse
+from app.services.image_search_service import ImageSearchService
+
+router = APIRouter()
+
+@router.post(
+    "/search-by-image",
+    response_model=BaseResponse,
+    summary="이미지 기반 종목 검색",
+    description="""
+                업로드된 이미지로부터 브랜드를 인식하여 종목 정보를 반환합니다.
+                최대 5MB 파일만 업로드 가능합니다.
+    """,
+    tags=["Stock"]
+)
+async def search_stock_by_image(
+        image: UploadFile = File(...),
+        db: Session = Depends(get_db)
+):
+    contents = await image.read()
+
+    result: ImageSearchResponse = ImageSearchService.search_stock_by_image(contents, db)
+
+    return success_response(
+        data=result,
+        status=SuccessStatus.IMAGE_SEARCH_SUCCESS
+    )
