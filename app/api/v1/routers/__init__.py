b417dbc2acc757fffcef61504377185720b37c77
Soomxn <sooming@duksung.ac.kr>
2025-08-18 14:12:10 +0900

    [FEAT] 전체 라우터에 감정 일기 추가 #17

--- app/api/v1/routers/__init__.py
+++ app/api/v1/routers/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from app.api.v1.routers import stock_router, backtest_router, pattern_detection_router
+from app.api.v1.routers import stock_router, backtest_router, pattern_detection_router, emotion_diary_router
 
 router = APIRouter()
 
 router.include_router(stock_router.router, prefix="/v1")
 router.include_router(backtest_router.router, prefix="/v1")
 router.include_router(pattern_detection_router.router, prefix="/v1")
-# router.include_router(emotion_diary_router.router, prefix="/v1")
+router.include_router(emotion_diary_router.router, prefix="/v1")
 
 # 아래에 추가 ex. router.include_router(user_router.router, prefix="/v1")
