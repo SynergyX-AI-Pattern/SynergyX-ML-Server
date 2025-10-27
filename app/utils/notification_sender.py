cb5141738fa57fa95041da847e3bc6d5488d3a8c
Soomxn <sooming@duksung.ac.kr>
2025-10-27 11:34:35 +0900

    [REFACTOR] 파라미터, url, payload 수정 #47

--- app/utils/notification_sender.py
+++ app/utils/notification_sender.py
@@ -4,13 +4,14 @@ from app.api_payload.code.error_status import ErrorStatus
 from app.core.config import settings
 from app.exceptions.base import APIException
 
-def send_notification_to_spring(title: str, message: str, notification_type: str):
+def send_notification_to_spring(user_id: int, title: str, message: str, notification_type: str):
     """
     SpringBoot 서버로 알림을 전송하는 유틸 함수입니다.
     """
     base_url = settings.SPRING_SERVER_BASE_URL
-    url = f"{base_url}/notifications/send"
+    url = f"{base_url}/test/notifications/send"
     payload = {
+        "userId": user_id,
         "title": title,
         "message": message,
         "type": notification_type
